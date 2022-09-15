10c3be728020a3800c48b45bac1505e3133b911d
unknown <greatboy_94@mail.ru>
2022-09-15 16:03:03 +0500

    Links part is done

--- DemoqaProject/pageObjects/Elements/Links.cs
+++ DemoqaProject/pageObjects/Elements/Links.cs
@@ -5,6 +5,14 @@ namespace DemoqaProject.PageObjects
 {
     public class Links : Elements
     {
+        public string createCode = "201";
+        public string noContentCode = "204";
+        public string movedCode = "301";
+        public string badRequestCode = "400";
+        public string unauthorizedCode = "401";
+        public string forbiddenCode = "403";
+        public string notFoundCode = "404";
+
         [FindsBy(How = How.Id, Using = "simpleLink")]
         public IWebElement homeLink;
         
@@ -13,6 +21,30 @@ namespace DemoqaProject.PageObjects
         
         [FindsBy(How = How.ClassName, Using = "banner-image")]
         public IWebElement bannerImage;
+        
+        [FindsBy(How = How.Id, Using = "created")]
+        public IWebElement createdButton;
+        
+        [FindsBy(How = How.Id, Using = "linkResponse")]
+        public IWebElement responseLink;
+        
+        [FindsBy(How = How.Id, Using = "no-content")]
+        public IWebElement noContentButton;
+        
+        [FindsBy(How = How.Id, Using = "moved")]
+        public IWebElement movedButton;
+        
+        [FindsBy(How = How.Id, Using = "bad-request")]
+        public IWebElement badRequestButton;
+        
+        [FindsBy(How = How.Id, Using = "unauthorized")]
+        public IWebElement unauthorizedButton;
+        
+        [FindsBy(How = How.Id, Using = "forbidden")]
+        public IWebElement forbiddenButton;
+        
+        [FindsBy(How = How.Id, Using = "invalid-url")]
+        public IWebElement notFoundButton;
 
         public void HomeLink()
         {
@@ -24,6 +56,41 @@ namespace DemoqaProject.PageObjects
         {
             home2Link.Click();
             driver.SwitchTo().Window(driver.WindowHandles[1]);
+        }
+        
+        public void Created()
+        {
+            createdButton.Click();
+        }
+
+        public void NoContent()
+        {
+            noContentButton.Click();
+        }
+        
+        public void Moved()
+        {
+            movedButton.Click();
+        }
+        
+        public void BadRequest()
+        {
+            badRequestButton.Click();
+        }
+        
+        public void Unauthorized()
+        {
+            unauthorizedButton.Click();
+        }
+        
+        public void Forbidden()
+        {
+            forbiddenButton.Click();
+        }
+        
+        public void NotFound()
+        {
+            notFoundButton.Click();
         }
     }    
 }
--- DemoqaProject/tests/ElementsTest/LinksTest.cs
+++ DemoqaProject/tests/ElementsTest/LinksTest.cs
@@ -28,5 +28,96 @@ namespace DemoqaProject
             links.Home2Link();
             Assert.IsTrue(links.bannerImage.Displayed);
         }
+        
+        [Test]
+        public void CheckCreated()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.Created();
+            Assert.That(links.responseLink.Text, Does.Contain(links.createCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckNoContent()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.NoContent();
+            Assert.That(links.responseLink.Text, Does.Contain(links.noContentCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckMoved()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.Moved();
+            Assert.That(links.responseLink.Text, Does.Contain(links.movedCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckBadRequest()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.BadRequest();
+            Assert.That(links.responseLink.Text, Does.Contain(links.badRequestCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckUnauthorized()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.Unauthorized();
+            Assert.That(links.responseLink.Text, Does.Contain(links.unauthorizedCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckForbidden()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.Forbidden();
+            Assert.That(links.responseLink.Text, Does.Contain(links.forbiddenCode));
+            Thread.Sleep(5000);
+        }
+        
+        [Test]
+        public void CheckNotFound()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            Links links = new Links();
+            links.NavigateToLinks();
+            
+            links.NotFound();
+            Assert.That(links.responseLink.Text, Does.Contain(links.notFoundCode));
+            Thread.Sleep(5000);
+        }
     }
 }
