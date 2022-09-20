567f1e4f98b7a9d9e97f6f104f2da8d49b54d91d
unknown <greatboy_94@mail.ru>
2022-09-20 14:44:24 +0500

    Upload is done

--- DemoqaProject/pageObjects/Elements/UploadAndDownload.cs
+++ DemoqaProject/pageObjects/Elements/UploadAndDownload.cs
@@ -6,10 +6,19 @@ namespace DemoqaProject.PageObjects
 {
     public class UploadAndDownload : Elements
     {
+        public string expectedText = "C:\\fakepath\\sampleFile.jpeg";
+        
         [FindsBy(How = How.Id, Using = "downloadButton")]
         public IWebElement downloadButton;
 
+        [FindsBy(How = How.Id, Using = "uploadFile")]
+        public IWebElement uploadButton;
+        
+        [FindsBy(How = How.Id, Using = "uploadedFilePath")]
+        public IWebElement uploadedFilePath;
+
         public string downloadURL = "C:\\Users\\great\\Downloads\\sampleFile.jpeg";
+        public string uploadURL = "C:\\Users\\great\\RiderProjects\\DemoqaProject\\DemoqaProject\\files\\sampleFile.jpeg";
 
 
         public void DownloadFile()
@@ -18,6 +27,11 @@ namespace DemoqaProject.PageObjects
             var options = new ChromeOptions();
             options.AddUserProfilePreference("download.default_directory", myDownloadFolder);
             downloadButton.Click();
+        }
+
+        public void UploadFile()
+        {
+            uploadButton.SendKeys(uploadURL);
         }
     }
 }
--- DemoqaProject/tests/ElementsTest/UploadAndDownloadTest.cs
+++ DemoqaProject/tests/ElementsTest/UploadAndDownloadTest.cs
@@ -16,5 +16,18 @@ namespace DemoqaProject
             uploadAndDownload.DownloadFile();
             Assert.IsTrue(File.Exists(uploadAndDownload.downloadURL));
         }
+        
+        [Test]
+        public void CheckUpload()
+        {
+            HomePage homePage = new HomePage();
+            homePage.NavigateToElementPage();
+            UploadAndDownload uploadAndDownload = new UploadAndDownload();
+            uploadAndDownload.NavigateToUploadAndDownload();
+            
+            uploadAndDownload.UploadFile();
+            Thread.Sleep(5000);
+            Assert.AreEqual(uploadAndDownload.expectedText, uploadAndDownload.uploadedFilePath.Text);
+        }
     }
 }
